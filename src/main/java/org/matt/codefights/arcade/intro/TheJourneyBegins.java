cff2bcdd2597f388603b36361edcf92ac79a0e07
Matthieu Dubois <matthieu.dubois@gmail.com>
2017-07-01 23:58:56 +0200

    CenturyFromYear implementation

--- src/main/java/org/matt/codefights/arcade/intro/TheJourneyBegins.java
+++ src/main/java/org/matt/codefights/arcade/intro/TheJourneyBegins.java
@@ -6,4 +6,14 @@ public class TheJourneyBegins {
     int add(int param1, int param2) {
         return param1 + param2;
     }
+
+    int centuryFromYear(int year) {
+
+        int r = year / 100;
+
+        if (year % 100 != 0) {
+            r++;
+        }
+        return r;
+    }
 }
--- src/test/java/org/matt/codefights/arcade/intro/TheJourneyBeginsTest.java
+++ src/test/java/org/matt/codefights/arcade/intro/TheJourneyBeginsTest.java
@@ -23,4 +23,49 @@ public class TheJourneyBeginsTest {
         assertEquals(1000, theJourneyBegins.add(0, 1000));
     }
 
+    @Test
+    public void centuryFromYearTest1() {
+        assertEquals(20, theJourneyBegins.centuryFromYear(1905));
+    }
+
+    @Test
+    public void centuryFromYearTest2() {
+        assertEquals(17, theJourneyBegins.centuryFromYear(1700));
+    }
+
+    @Test
+    public void centuryFromYearTest3() {
+        assertEquals(20, theJourneyBegins.centuryFromYear(1988));
+    }
+
+    @Test
+    public void centuryFromYearTest4() {
+        assertEquals(20, theJourneyBegins.centuryFromYear(2000));
+    }
+
+    @Test
+    public void centuryFromYearTest5() {
+        assertEquals(21, theJourneyBegins.centuryFromYear(2001));
+    }
+
+    @Test
+    public void centuryFromYearTest6() {
+        assertEquals(2, theJourneyBegins.centuryFromYear(200));
+    }
+
+    @Test
+    public void centuryFromYearTest7() {
+        assertEquals(4, theJourneyBegins.centuryFromYear(374));
+    }
+
+    @Test
+    public void centuryFromYearTest8() {
+        assertEquals(1, theJourneyBegins.centuryFromYear(45));
+    }
+
+    @Test
+    public void centuryFromYearTest9() {
+        assertEquals(1, theJourneyBegins.centuryFromYear(8));
+    }
+
 }
